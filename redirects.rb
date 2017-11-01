c117fa85c4498211429917fdc42f59be5ef3f917
ghanna2017 <ghanna@pivotal.io>
2017-11-01 12:24:27 +0000

    fixes redirects for previous versions

--- redirects.rb
+++ redirects.rb
@@ -1,1 +1,5 @@
 r301 %r{/addon-ipsec/(?![\d-]+)(.*)}, "/addon-ipsec/1-8/$1"
+
+r301 %r{/addon-ipsec/1-7-[\d]+/(.*)}, "/addon-ipsec/1-7/$1"
+r301 %r{/addon-ipsec/1-6-[\d]+/(.*)}, "/addon-ipsec/1-6/$1"
+r301 %r{/addon-ipsec/1-5-[\d]+/(.*)}, "/addon-ipsec/1-5/$1"
