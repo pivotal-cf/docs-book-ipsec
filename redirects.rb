caf1497b3052c91dc4797b1fe0717189a8b14fb8
Ben Tarnoff <btarnoff@pivotal.io>
2018-01-19 15:48:40 -0500

    remove 1.6, add redirect, add pdf to dropdown[#153832603]

--- redirects.rb
+++ redirects.rb
@@ -1,5 +1,5 @@
 r301 %r{/addon-ipsec/(?![\d-]+)(.*)}, "/addon-ipsec/1-8/$1"
 
 r301 %r{/addon-ipsec/1-7-[\d]+/(.*)}, "/addon-ipsec/1-7/$1"
-r301 %r{/addon-ipsec/1-6-[\d]+/(.*)}, "/addon-ipsec/1-6/$1"
-r301 %r{/addon-ipsec/1-5-[\d]+/(.*)}, "/addon-ipsec/1-5/$1"
+
+r301 %r{/addon-ipsec/1-6/(.*)}, 'http://docs.pivotal.io/archives/addon-ipsec-1.6.pdf'
