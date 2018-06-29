006f877c438f7d6d0d61676d761eecde3d33b240
jncd <jday@pivotal.io>
2018-06-29 09:31:43 -0700

    updates redirects to default to 1.9

--- redirects.rb
+++ redirects.rb
@@ -1,4 +1,6 @@
-r301 %r{/addon-ipsec/(?![\d-]+)(.*)}, "/addon-ipsec/1-8/$1"
+r301 %r{/addon-ipsec/(?![\d-]+)(.*)}, "/addon-ipsec/1-9/$1"
+
+r301 %r{/addon-ipsec/1-8-[\d]+/(.*)}, "/addon-ipsec/1-8/$1"
 
 r301 %r{/addon-ipsec/1-7-[\d]+/(.*)}, "/addon-ipsec/1-7/$1"
 
