e564a5140a3a96c56850092d48a5517d6b5fcdb2
animatedmax <mhufnagel@pivotal.io>
2018-08-09 15:59:12 -0700

    Redirect http traffic to https on production

--- redirects.rb
+++ redirects.rb
@@ -1,3 +1,8 @@
+# Redirect all production http traffic to https
+r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
+  rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
+}
+
 r301 %r{/addon-ipsec/(?![\d-]+)(.*)}, "/addon-ipsec/1-9/$1"
 
 r301 %r{/addon-ipsec/1-8-[\d]+/(.*)}, "/addon-ipsec/1-8/$1"
