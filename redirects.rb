85728d85df6b72b5ad23219d49227898b5096a19
Richard Johnson <rjohnson@pivotal.io>
2019-01-24 16:46:47 -0800

    Added catch-all redirection [#163159580]

--- redirects.rb
+++ redirects.rb
@@ -7,7 +7,7 @@ r301 %r{/addon-ipsec/(?![\d-]+)(.*)}, "/addon-ipsec/1-9/$1"
 
 r301 %r{/addon-ipsec/1-8-[\d]+/(.*)}, "/addon-ipsec/1-8/$1"
 
-r301 %r{/addon-ipsec/1-7-[\d]+/(.*)}, "/addon-ipsec/1-7/$1"
+r301 %r{/addon-ipsec/1-7/(.*)}, 'http://docs.pivotal.io/archives/addon-ipsec-1.7.pdf'
 
 r301 %r{/addon-ipsec/1-6/(.*)}, 'http://docs.pivotal.io/archives/addon-ipsec-1.6.pdf'
 
