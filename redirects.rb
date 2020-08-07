a003e81f5f8a2c21426718daf02a41ed51b7a7e7
Chris Wong <cwong@pivotal.io>
2020-08-07 16:58:00 -0400

    redirect to hide CredHub? - attempt 5 [#172591290]

--- redirects.rb
+++ redirects.rb
@@ -2,3 +2,5 @@
 r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
+
+r301 %r{/addon-ipsec/1-9/credhub.html}, '/addon-ipsec/1-9/index.html'
