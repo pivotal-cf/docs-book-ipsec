e6bf9d62efec0c0748f41ec4c43be30ca526d2b8
Samia Nneji <snneji@vmware.com>
2022-05-26 09:52:34 +0100

    IPsec adding more redirects [#182195205]

--- redirects.rb
+++ redirects.rb
@@ -6,3 +6,5 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
 # Redirect from docs.pivotal.io/addon-ipsec/1-9 to docs.vmware.com build service docs
 
 r301  %r{/addon-ipsec/1-9/(.*)}, "https://docs.vmware.com/en/IPsec-for-VMware-Tanzu/1.9/documentation/GUID-$1"
+r301  %r{/addon-ipsec/1-9/}, "https://docs.vmware.com/en/IPsec-for-VMware-Tanzu/1.9/documentation/GUID-index.html"
+r301  %r{/addon-ipsec/1-9}, "https://docs.vmware.com/en/IPsec-for-VMware-Tanzu/1.9/documentation/GUID-index.html"
